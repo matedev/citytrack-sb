7ec66544731d80c6b2a71e41a158617a3bef1ed5
Máté Kis-Király <Mt Kis-Kirly>
2016-10-31 10:06:30 +0200

    Change project structure

---
+++ inc/partners.php
@@ -0,0 +1,42 @@
+    <div class="container separator">
+            <h1 class="separator-content">CityTrack Partners</h1>
+    </div>
+
+    <div id="partner-block" class="block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-uta.jpg">
+                </div>
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-finnpark.jpg">
+                </div>
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-tampere.jpg">
+                </div>
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-talo.jpg">
+                </div>
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-sryhma.jpg">
+                </div>
+                <div class="col-md-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-beemray.jpg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2 col-md-push-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-tredea.jpg">
+                </div>
+                <div class="col-md-2 col-md-push-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-here.jpg">
+                </div>
+                <div class="col-md-2 col-md-push-2 col-sm-4 img-container">
+                    <img class="img-class" src="img/logo-kesko.jpg">
+                </div>
+                <div class="col-md-2 col-md-push-2 col-sm-4 col-sm-push-4 img-container">
+                    <img class="img-class" src="img/logo-allbright.jpg">
+                </div>
+            </div>
+        </div>
+    </div>

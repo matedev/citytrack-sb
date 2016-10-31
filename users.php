887e4148ee10bf0707f9e18713444e449a9c4d93
Máté Kis-Király <Mt Kis-Kirly>
2016-10-31 15:11:01 +0200

    Add users.php

---
+++ users.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CityTrack</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/>
+</head>
+<body>
+
+<?php include 'inc/menu.php';?>
+
+<div id="blog">
+    <div class="splash-block">
+    </div>
+
+        <div class="container">
+
+            <div class="separator">
+                <h1 class="separator-content"><img src="img/logo.png" height="63px"></h1>
+            </div>
+
+            <div id="posts" class="block">
+                <div class="row apost">
+                    <div class="post-header col-md-12">
+                            <p>Posted on <span id="post-date">16.10.2016</span>
+                            <strong> by <span id="post-author">XY</span></strong></p>
+                        
+                    </div>
+                    <div class="post-img col-md-5">
+                        <div class="img-container text-center">
+                            <img class="img-responsive" src="img/blog-img.jpg">
+                        </div>
+                    </div>
+                    <div class="post-body col-md-7">
+                        <h1 class="post-title text-blue">
+                            This is the title of a blog post
+                        </h1>
+                        <p class="post-text demo1">
+                            CityTrack project will become an unique urban test site for the design, 
+                            development and piloting of modern mobility services and service-rich 
+                            dense urban venues. CityTrack helps companies create a highly 
+                            personalized,seamless and data-driven cross-channel event....                                                                       
+                        </p>
+                        <p class="text-right">
+                            <button type="button" class="btn btn-blue" data-toggle="" href=''>Read more</button>
+                        </p>
+                    </div>
+                </div>
+                <div class="row apost">
+                    <div class="post-header col-md-12">
+                            <p>Posted on <span id="post-date">16.10.2016</span>
+                            <strong> by <span id="post-author">XY</span></strong></p>
+                        
+                    </div>
+                    <div class="post-img col-md-5">
+                        <div class="img-container text-center">
+                            <img class="img-responsive" src="img/blog-img.jpg">
+                        </div>
+                    </div>
+                    <div class="post-body col-md-7">
+                        <h1 class="post-title text-blue">
+                            This is the title of a blog post
+                        </h1>
+                        <p class="post-text demo1">
+                            CityTrack project will become an unique urban test site for the design, 
+                            development and piloting of modern mobility services and service-rich 
+                            dense urban venues. CityTrack helps companies create a highly 
+                            personalized,seamless and data-driven cross-channel event....                                                                       
+                        </p>
+                        <p class="text-right">
+                            <button type="button" class="btn btn-blue" data-toggle="" href=''>Read more</button>
+                        </p>
+                    </div>
+                </div>
+                <div class="row apost">
+                    <div class="post-header col-md-12">
+                            <p>Posted on <span id="post-date">16.10.2016</span>
+                            <strong> by <span id="post-author">XY</span></strong></p>
+                        
+                    </div>
+                    <div class="post-img col-md-5">
+                        <div class="img-container text-center">
+                            <img class="img-responsive" src="img/blog-img.jpg">
+                        </div>
+                    </div>
+                    <div class="post-body col-md-7">
+                        <h1 class="post-title text-blue">
+                            This is the title of a blog post
+                        </h1>
+                        <p class="post-text demo1">
+                            CityTrack project will become an unique urban test site for the design, 
+                            development and piloting of modern mobility services and service-rich 
+                            dense urban venues. CityTrack helps companies create a highly 
+                            personalized,seamless and data-driven cross-channel event....                                                                       
+                        </p>
+                        <p class="text-right">
+                            <button type="button" class="btn btn-blue" data-toggle="" href=''>Read more</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    
+
+    <?php include 'inc/partners.php';?>
+    <?php include 'inc/footer.php';?>
+    
+</body>
+</html>

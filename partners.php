0b7e333ffa9afbffaad9ef5f2b6645c356722b37
Máté Kis-Király <Mt Kis-Kirly>
2016-11-01 15:53:04 +0200

    Add header.php

--- partners.php
+++ partners.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>CityTrack</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-    <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <meta name="mobile-web-app-capable" content="yes">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap-theme.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/fonts.css">
-    <link rel="stylesheet" href="assets/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/>
-</head>
-<body>
-
+<?php include 'inc/header.php';?>
 <?php include 'inc/menu.php';?>
 
 <div id="partners">
